4c6800212e9fff7b32de0e96768f1700c9897deb
OtomeFavList <otomefavlist@163.com>
2026-09-14 13:43:26 +0800

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,3 @@
-// 自动加载data/games目录下所有游戏JS，聚合为全局 gameTemplateList
-// 新增游戏仅需要在data/games新建文件，无需修改此处
+// 自动加载 data/games 目录下所有游戏 JS ，聚合为全局 gameTemplateList
+// 新增游戏仅需要在 data/games 新建文件，无需修改此处
 window.gameTemplateList = window.gameTemplateList || [];
