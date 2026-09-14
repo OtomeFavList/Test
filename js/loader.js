325d969e37c54e8501df5db33d2499144d9acc05
OtomeFavList <otomefavlist@163.com>
2026-09-14 13:52:15 +0800

    Update comment for clarity on game file addition

--- js/loader.js
+++ js/loader.js
@@ -1,3 +1,3 @@
 // 自动加载 data/games 目录下所有游戏 JS ，聚合为全局 gameTemplateList
-// 新增游戏仅需要在 data/games 新建文件，无需修改此处
+// 新增游戏仅需要在 data/games 新建文件，无需修改本文件
 window.gameTemplateList = window.gameTemplateList || [];
