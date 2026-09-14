c3e32c9aa1383715788dd163dd53180e83920899
OtomeFavList <otomefavlist@163.com>
2026-09-14 13:49:54 +0800

    Update games.js

--- data/games.js
+++ data/games.js
@@ -1,6 +1,6 @@
 // data/games.js
 // 聚合 games/ 下所有独立游戏数据，自动合并全局数组
-// 新增游戏只新建 games/gameXXX.js，仅追加文件名，其余代码永久不用修改
+// 仅追加文件名，其余代码永久不修改
 const allGameFileNames = [
     "game001.js", "game002.js", "game003.js", "game004.js", "game005.js",
     "game006.js", "game007.js", "game008.js", "game009.js", "game010.js",
